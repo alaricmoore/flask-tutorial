5d1c69e6c8e740980d52c059b922f935abe8a520
Carlos Cardoso <cadusk@yahoo.com.br>
2013-01-03 22:58:31 -0200

    Part VI

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,9 @@
-from flask.ext.wtf import Form, TextField, BooleanField
-from flask.ext.wtf import Required
+from flask.ext.wtf import Form, TextField, BooleanField, TextAreaField
+from flask.ext.wtf import Required, Length
+
+class EditForm(Form):
+    nickname = TextField('nickname', validators=[Required()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
